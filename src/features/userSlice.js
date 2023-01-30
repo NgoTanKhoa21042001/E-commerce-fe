ee361f131b1a201bcf3de744051ab3a1dc32697f
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-01-30 16:57:51 +0700

    Create productSlice, userSlice, and store

---
+++ src/features/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = null;
+
+export const userSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
